c3d2482b17cc332fcab827e45e58ba0fcc764c42
BlackStar <sthefanya86@gmail.com>
2023-05-16 18:10:25 -0300

    Ajustes no js

--- assets/js/backgound.js
+++ assets/js/backgound.js
@@ -4,4 +4,12 @@ let btn = document.getElementById('button').addEventListener('click', () => {
     
     let corpo = document.getElementById('corpo');
     corpo.style.background = iValue;
+    input.style.border = iValue;
+
+    if(iValue == '#000000') {
+        let title = document.getElementById('title');
+        title.style.color = '#ffffff';
+    } else {
+        title.style.color = '#000000';
+    }
 });
